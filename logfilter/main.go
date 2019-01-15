2476196212e702db506de196dc97db9bf978756a
Siddardh Padyala <siddardhp@agrostar.in>
2019-01-15 14:55:41 +0530

    Log Filter added

--- logfilter/main.go
+++ logfilter/main.go
@@ -2,38 +2,104 @@ package main
 
 import (
 	"bufio"
-	"flag"
+
+	// "flag"
+	// // "flag"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
+
+	flags "github.com/jessevdk/go-flags"
 )
 
-var threshold = flag.Int("tl", 3000, "Time limit above which the queries have to be monitored")
+// var threshold = flag.Int("tl", 3000, "Time limit above which the queries have to be monitored")
+
+var opts struct {
+	Slow         int      `long:"slow" description:"Returns lines which are slower than the value in ms"`
+	NonIndex     bool     `long:"ni" description:"Queries that aren't indexed"`
+	KeysExamined int      `long:"keysExamined" description:"Returns Queries which have examined more keys than this"`
+	Keywords     []string `short:"w" long:"keywords" description:"Keywords to track"`
+	MultiIndex   bool     `long:"mi" description:"Queries using multiple indexes"`
+}
 
 func handleLine(line string) {
 	if !strings.Contains(line, "COMMAND") {
 		return
 	}
 	lexems := strings.Split(line, " ")
-	// timestamp := lexems[0]
 	tts := lexems[len(lexems)-1]
 	tts = strings.Trim(tts, "\n")
 	tts = tts[:len(tts)-2]
-	// tt, err := strconv.Atoi(tts)
-	// if err != nil {
-	// 	continue
+	tt, err := strconv.Atoi(tts)
+	if err != nil {
+		return
+	}
+	// if opts.Slow != 0 && tt > opts.Slow {
+	// 	fmt.Print(line)
+	// 	return
 	// }
-	// if tt > *threshold {
-	// 	fmt.Println(timestamp, tts)
-	// 	fmt.Println(line.Text)
-	// 	break
-	// }
-	fmt.Println(getStringStartingWith(lexems, "protocol"))
+	ke, err := strconv.Atoi(getDataStartingWith(lexems, "keysExamined"))
+	if err != nil {
+		return
+	}
+	if checkKeyword(line) && slowQuery(tt) && (nonIndexed(line) || multiIndexed(line)) && keysExamined(ke) {
+		fmt.Print(line)
+	}
+}
+
+func checkKeyword(line string) bool {
+	if len(opts.Keywords) == 0 {
+		return true
+	}
+	for _, v := range opts.Keywords {
+		if strings.Contains(line, v) {
+			// fmt.Print(line)
+			return true
+		}
+	}
+	return false
+}
+
+func nonIndexed(line string) bool {
+	if !opts.NonIndex {
+		return true
+	}
+	return !strings.Contains(line, "IXSCAN")
+}
+
+func multiIndexed(line string) bool {
+	if !opts.MultiIndex {
+		return true
+	}
+	return strings.Count(line, "IXSCAN") > 1
+}
+
+func keysExamined(ke int) bool {
+	if opts.KeysExamined == 0 {
+		return true
+	}
+	return ke > opts.KeysExamined
+}
+
+func slowQuery(t int) bool {
+	if opts.Slow == 0 {
+		return true
+	}
+	return t > opts.Slow
 }
 
 func main() {
-	flag.Parse()
+	// flag.Parse()
+	_, err := flags.Parse(&opts)
+	if flags.WroteHelp(err) {
+		os.Exit(0)
+	}
+	if opts.NonIndex && opts.MultiIndex {
+		panic(fmt.Errorf("ni and mi can't be set at the same time"))
+	}
+	// log.Printf("%+v\n", opts)
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
@@ -44,6 +110,7 @@ func main() {
 			continue
 		}
 		handleLine(line)
+		// break
 	}
 }
 
@@ -57,4 +124,13 @@ func getStringStartingWith(from []string, target string) string {
 		}
 	}
 	return ""
+}
+
+func getDataStartingWith(from []string, target string) string {
+	s := getStringStartingWith(from, target)
+	if s == "" {
+		return ""
+	}
+	target = target + ":"
+	return strings.Replace(s, target, "", 1)
 }
